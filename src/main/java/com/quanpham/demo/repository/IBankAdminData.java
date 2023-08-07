1325caf04b344d89ad94225505393a6b6541444e
Hoang Quan <kjwjnpham@gmail.com>
2023-08-07 11:41:13 +0700

    update

--- src/main/java/com/quanpham/demo/repository/IBankAdminData.java
+++ src/main/java/com/quanpham/demo/repository/IBankAdminData.java
@@ -14,18 +14,7 @@ public interface IBankAdminData extends JpaRepository<BankAdmin, Long> {
     Optional<BankAdmin> findByEmail(String email);
 
     @Query(value = "select * from users u where u.id in(select up.id_user  from users_product up where up.id_product = :idProduct", nativeQuery = true)
-    List<BankAdmin> findByIdProduct(@Param("idProduct") Long idProduct);
 
-    // @Query(value = "\n" + //
-    // "select * from users u where u.id in" +
-    // "(select u.id from users u where u.id in(select up.id_user from users_product
-    // up where up.id_product = :idProduct))"
-    // +
-    // " and u.id in(select tc.id from trans_counter tc where tc.num_of_ticket <10);
-    // \n" + //
-    // "", nativeQuery = true)
-    // List<BankAdmin> findByIdProductAndNumOfTicket(@Param("idProduct") Long
-    // idProduct);
-    // List<Roles> findRoles_BankAdmin(BankAdmin bankAdmin);
+    List<BankAdmin> findByIdProduct(@Param("idProduct") Long idProduct);
 
 }
